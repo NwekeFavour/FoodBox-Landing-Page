6752628e3dadef9b6e4894620151d0f9d5f06d40
Oknown15 <buchijunior90@gmail.com>
2024-04-26 00:21:28 +0100

    Finally done with the full landing page

---
+++ assets/javascript/index.js
@@ -0,0 +1,9 @@
+function Navigation() {
+    document.getElementById('navbar').style.display = "block";
+    document.getElementById('navigation').style.display = "block";
+}
+
+function CloseBtn(){
+    document.getElementById('navbar').style.display = "none";
+    document.getElementById('navigation').style.display = "none";
+}
